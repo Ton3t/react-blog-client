8ea6dbe394e1d7b30d02334c0dd94fc76f08c273
Ton3t <elton3t@gmail.com>
2024-04-08 00:38:42 +0200

    organizar carpetas y archivos

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/misc/Navbar';
-import Home from './components/Home';
-import Articulos from './components/Articulos';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/misc/Navbar";
+import Home from "./components/home/Home";
+import Articulos from "./components/posts/Articulos";
 
 function Router() {
   return (
     <BrowserRouter>
-    <Navbar />
-        <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/articulos' element={<Articulos />} />
-            <Route path='/login'>Login</Route>
-            <Route path='/register'>Register</Route>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/articulos" element={<Articulos />} />
+        <Route path="/login">Login</Route>
+        <Route path="/register">Register</Route>
+      </Routes>
     </BrowserRouter>
-)
+  );
 }
 
 export default Router;
