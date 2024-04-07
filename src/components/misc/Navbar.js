03855abd6669129d32a0068375cf23566f32d85f
Ton3t <elton3t@gmail.com>
2024-04-07 18:26:56 +0200

    link inicio añadido al navbar

--- src/components/misc/Navbar.js
+++ src/components/misc/Navbar.js
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <div className='navbar'>
         <Link to="/"><h1>Toneti Blog</h1></Link>
+        <Link to="/">Inicio</Link>
         <Link to="/articulos">Artículos</Link>
         <Link to="/login">Log In</Link>
         <Link to="/register">Registrate</Link>
